8e5340a1b95a54e397355f4efcf50cf30d7d2742
Tri <bdtcloner1@gmail.com>
2025-04-11 08:14:19 +0700

    update folder

--- routes/searchs.js
+++ routes/searchs.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { search } = require('../utils/search');
-
+let { check_authentication } = require('../utils/check_auth');
 // GET /search/:key
-router.get('/:key', (req, res) => {
+router.get('/:key',check_authentication, (req, res) => {
   const keyword = req.params.key;
 
   if (!keyword || keyword.trim() === '') {
